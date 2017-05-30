62ae4e755264d8d44945143ddb50b350d54f3725
Matthew <Matthew@mlwdesktop>
2017-05-30 09:13:30 -0500

    Initial commit of base project

---
+++ src/main/java/site/swgoh/swgoh_lambda/myHandler/Handler.java
@@ -0,0 +1,16 @@
+package site.swgoh.swgoh_lambda.myHandler;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.S3Event;
+
+//TODO adjust input and output types of function
+public class Handler implements RequestHandler<S3Event, String> {
+
+	@Override
+	public String handleRequest(S3Event event, Context context) {
+		// TODO implement handler
+		return "Done executing handler";
+	}
+
+}
